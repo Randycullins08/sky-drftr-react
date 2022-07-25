ac232ae9ab46177180f99db011454ba3ad983da5
Randy Cullins <randycullins2018@gmail.com>
2022-07-25 15:59:37 -0600

    Mobile part of navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,40 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
 export default function Navbar() {
+  const [toggle, setToggle] = useState(true);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  const toggleNav = () => {
+    setToggle((toggle) => !toggle);
+  };
+
+  useEffect(() => {
+    const changeWidth = () => {
+      setScreenWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", changeWidth);
+
+    return () => {
+      window.removeEventListener("resize", changeWidth);
+    };
+  }, []);
+
   return (
     <div className="navbar-container">
-      <div className="navbar-links-wrapper">
-        <h2>Home</h2>
-        <h2>Bio</h2>
-        <h2>Music</h2>
-        <h2>Shows</h2>
-        <h2>Contact</h2>
-      </div>
+      {(toggle || screenWidth > 768) && (
+        <div className="navbar-links-wrapper">
+          <h2>HOME</h2>
+          <h2>BIO</h2>
+          <h2>MUSIC</h2>
+          <h2>SHOWS</h2>
+          <h2>CONTACT</h2>
+        </div>
+      )}
+      <i
+        className={toggle ? "fas fa-times" : "fas fa-bars"}
+        onClick={toggleNav}
+      ></i>
     </div>
   );
 }
