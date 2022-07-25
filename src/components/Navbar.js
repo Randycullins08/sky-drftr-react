ee1849de9139ec409836e0a9ee44bb6a5e1d2103
Randy Cullins <randycullins2018@gmail.com>
2022-07-25 08:45:09 -0600

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+// import React from "react";
+
+// export default function Navbar() {
+//   return (
+//     <div className="navbar-container">
+//       <div className="logo-wrapper">
+//         <img src="/img/SKYDRFTR-LOGO-WHITE-W-BLACK-BG.png" alt="Logo" />
+//       </div>
+
+//       <div>
+//         <ul>
+//           <li>Home</li>
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// }
