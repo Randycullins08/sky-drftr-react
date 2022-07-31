a00f2b040d0b352fed6e988192f01c2fa8a2c691
Randy Cullins <randycullins2018@gmail.com>
2022-07-31 17:56:01 -0600

    Start of Hero Section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import "./components/Navbar";
 import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <HeroSection />
     </div>
   );
 }
---
+++ src/components/HeroSection.js
@@ -0,0 +1,57 @@
+import { Link } from "react-router-dom";
+
+export default function HeroSection() {
+  return (
+    <div className="hero-section-container">
+      <img
+        src="/img/SKYDRFTR-LOGO-COSMIC-W-TRANSPARENT-BG.png"
+        alt="band logo"
+      />
+
+      <div className="social-links-wrapper">
+        <Link
+          to={{ pathname: "https://www.facebook.com/SkyDRFTR/" }}
+          target="_blank"
+        >
+          <i className="fab fa-facebook"></i>
+        </Link>
+        <Link
+          to={{ pathname: "https://www.instagram.com/skydrftr/" }}
+          target="_blank"
+        >
+          <i className="fab fa-instagram"></i>
+        </Link>
+        <Link
+          to={{ pathname: "https://twitter.com/skydrftrmusic/" }}
+          target="_blank"
+        >
+          <i className="fab fa-twitter"></i>
+        </Link>
+        <Link
+          to={{ pathname: "https://www.tiktok.com/@skydrftr?" }}
+          target="_blank"
+        >
+          <i className="fab fa-tiktok"></i>
+        </Link>
+        <Link
+          to={{
+            pathname:
+              "https://www.youtube.com/channel/UCqETSavnuiDw7POcrUKLiZQ",
+          }}
+          target="_blank"
+        >
+          <i className="fab fa-youtube"></i>
+        </Link>
+        <Link
+          to={{
+            pathname:
+              "https://open.spotify.com/artist/2a4hFh89XbgUtTYdFHTQFW?utm_source=embed_v2&go=1&play=1&nd=1",
+          }}
+          target="_blank"
+        >
+          <i className="fab fa-spotify"></i>
+        </Link>
+      </div>
+    </div>
+  );
+}
