f9fed9d4d4e05846107294db545d29e3f19c4047
Randy Cullins <randycullins2018@gmail.com>
2022-10-18 14:36:17 -0600

    added flyer

--- src/sliderData/flyerImages.js
+++ src/sliderData/flyerImages.js
@@ -1,24 +1,30 @@
 export const flyerImages = [
   {
     id: 0,
+    image: "/img/elko-show.jpg",
+    alt: "Elko Show",
+  },
+  {
+    id: 1,
     image: "/img/dead_animal_show.jpg",
     alt: "Dead Animal Show",
   },
 
   {
-    id: 1,
+    id: 2,
     image: "/img/feb-show.jpeg",
     alt: "Feburary Show",
   },
 
   {
-    id: 2,
+    id: 3,
     image: "/img/LBS-show.jpeg",
     alt: "LBS Show",
   },
 
   {
-    id: 3,
+    id: 4,
     image: "/img/summer-daze-show.jpg",
+    alt: "Sumer Daze Show",
   },
 ];
